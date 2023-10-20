ae832ef620022382af877ba6789d5101b63adbdf
ScrimBundy <tbutler0249@gmail.com>
2023-10-20 15:04:27 -0400

    Restructure minimax

--- minimax_package/draft_minimax.py
+++ minimax_package/draft_minimax.py
@@ -19,16 +19,13 @@ class DraftNode:
     def __init__(
             self, blue_bans, red_bans,
             blue_picks, red_picks, order_index,
-            evaluation_function,
-            alpha=-np.inf, beta=np.inf):
+            evaluation_function):
         self.blue_bans = blue_bans
         self.red_bans = red_bans
         self.blue_picks = blue_picks
         self.red_picks = red_picks
         self.order_index = order_index
         self.evaluate = evaluation_function
-        self.alpha = alpha
-        self.beta = beta
 
     def is_terminal(self):
         return self.order_index >= 20
@@ -59,16 +56,7 @@ def iterative_deepening(node, max_depth, top_n=10):
     return top_moves
 
 
-def minimax_for_blue(node, depth, max_depth, order_index, alpha, beta):
-    """
-    Calculates the best next action for blue to take on a given draft round
-    :param node: search space node to evaluate
-    :param depth: depth of the current node
-    :param order_index: index of the draft order
-    :param alpha: The best value found so far for the maximizing team
-    :param beta: The best value found so far for the minimizing team
-    :return:
-    """
+def minimax(maximize_blue, node, depth, max_depth, order_index, alpha, beta):
     best_action = None
     if depth == 0 or node.is_terminal():
         return node.evaluate()
@@ -81,55 +69,83 @@ def minimax_for_blue(node, depth, max_depth, order_index, alpha, beta):
     current_team = __team_order[order_index]
     order_index += 1
 
-    if current_action == 0:
-        # Ban
-        if current_team == 0:
-            # Blue ban
-            max_eval = -np.inf
-            for child in node.children:
-                _, eval = minimax_for_blue(child, depth - 1, order_index, alpha, beta)
-                if eval > max_eval:
-                    max_eval = eval
-                    best_action = child  # Store the best action
-                alpha = max(alpha, eval)
-                if alpha >= beta:
-                    break  # Prune the rest of the subtree
-            return best_action, max_eval
-        else:
-            # Red ban
-            min_eval = np.inf
-            for child in node.children:
-                _, eval = minimax_for_blue(child, depth - 1, order_index, alpha, beta)
-                if eval < min_eval:
-                    min_eval = eval
-                    best_action = child  # Store the best action
-                beta = min(beta, eval)
-                if alpha >= beta:
-                    break  # Prune the rest of the subtree
-            return best_action, min_eval
+    if maximize_blue:
+        return minimax_for_blue(node, depth, max_depth, order_index, alpha, beta, current_team, current_action)
     else:
-        # Pick
-        if current_team == 0:
-            # Blue pick
-            max_eval = -np.inf
-            for child in node.children:
-                _, eval = minimax_for_blue(child, depth - 1, order_index, alpha, beta)
-                if eval > max_eval:
-                    max_eval = eval
-                    best_action = child  # Store the best action
-                alpha = max(alpha, eval)
-                if alpha >= beta:
-                    break  # Prune the rest of the subtree
+        return minimax_for_red(node, depth, max_depth, order_index, alpha, beta, current_team, current_action)
+
+
+def minimax_for_blue(node, depth, max_depth, order_index, alpha, beta, current_team, current_action):
+    """
+    Calculates the best next action for blue to take on a given draft round
+    :param node: search space node to evaluate
+    :param depth: depth of the current node
+    :param max_depth: depth of an early termination layer
+    :param order_index: index of the draft order
+    :param alpha: The best value found so far for the maximizing team
+    :param beta: The best value found so far for the minimizing team
+    :return:
+    """
+    best_action = None
+    if current_team == 0:
+        # Blue
+        max_eval = -np.inf
+        for child in node.children:
+            _, eval = minimax(True, child, depth + 1, max_depth, order_index, alpha, beta)
+            if eval > max_eval:
+                max_eval = eval
+                best_action = child  # Store the best action
+            alpha = max(alpha, eval)
+            if alpha >= beta:
+                break  # Prune the rest of the subtree
             return best_action, max_eval
-        else:
-            # Red pick
-            min_eval = np.inf
-            for child in node.children:
-                _, eval = minimax_for_blue(child, depth - 1, order_index, alpha, beta)
-                if eval < min_eval:
-                    min_eval = eval
-                    best_action = child  # Store the best action
-                beta = min(beta, eval)
-                if alpha >= beta:
-                    break  # Prune the rest of the subtree
-            return best_action, min_eval
+    else:
+        # Red
+        min_eval = np.inf
+        for child in node.children:
+            _, eval = minimax(True, child, depth + 1, max_depth, order_index, alpha, beta)
+            if eval < min_eval:
+                min_eval = eval
+                best_action = child  # Store the best action
+            beta = min(beta, eval)
+            if alpha >= beta:
+                break  # Prune the rest of the subtree
+        return best_action, min_eval
+
+
+def minimax_for_red(node, depth, max_depth, order_index, alpha, beta, current_team, current_action):
+    """
+    Calculates the best next action for blue to take on a given draft round
+    :param node: search space node to evaluate
+    :param depth: depth of the current node
+    :param max_depth: depth of an early termination layer
+    :param order_index: index of the draft order
+    :param alpha: The best value found so far for the maximizing team
+    :param beta: The best value found so far for the minimizing team
+    :return:
+    """
+    best_action = None
+    if current_team == 0:
+        # Blue
+        min_eval = np.inf
+        for child in node.children:
+            _, eval = minimax(False, child, depth + 1, max_depth, order_index, alpha, beta)
+            if eval < min_eval:
+                min_eval = eval
+                best_action = child  # Store the best action
+            beta = min(beta, eval)
+            if alpha >= beta:
+                break  # Prune the rest of the subtree
+        return best_action, min_eval
+    else:
+        # Red
+        max_eval = -np.inf
+        for child in node.children:
+            _, eval = minimax(False, child, depth + 1, max_depth, order_index, alpha, beta)
+            if eval > max_eval:
+                max_eval = eval
+                best_action = child  # Store the best action
+            alpha = max(alpha, eval)
+            if alpha >= beta:
+                break  # Prune the rest of the subtree
+        return best_action, max_eval
